b40cf59d5a2b27672911e1e194149cb18f4e1557
Tim Lobes <timlobes@gmail.com>
2014-12-22 23:27:54 -0800

    Remove response method in favor of passing as parameter

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 var http = require("http");
 var url = require("url");
 
-
 function start(route, handle) {
-	function onRequest(request, response) {
-		var pathname = url.parse(request.url).pathname;
-		console.log("Request for " + pathname + " received.");
+  	function onRequest(request, response) {
+    	var pathname = url.parse(request.url).pathname;
+    	console.log("Request for " + pathname + " received.");
 
-		route(handle, pathname);
+    	route(handle, pathname, response);
+  	}
 
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write("Hello World");
-		response.end();
-	}
-
-	http.createServer(onRequest).listen(3000);
-	console.log("Server has started.");
+  	http.createServer(onRequest).listen(8888);
+  	console.log("Server has started.");
 }
 
 exports.start = start;
