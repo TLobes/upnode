17fa1b9fa05bfefa1a3afb178ba2c6142b4f5dee
Tim Lobes <timlobes@gmail.com>
2014-12-26 00:07:05 -0800

    Add upload button. Install formidable

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,6 @@
-var generateBody = require("./generateBody");
-var querystring = require("querystring");
+var generateBody = require("./generateBody"), 
+	querystring = require("querystring"),
+    fs = require("fs");
 
 function start(response, postData) {
 	console.log("Request handler 'start' was called.");
@@ -8,11 +9,28 @@ function start(response, postData) {
 }
 
 function upload(response, postData) {
-	console.log("Request handler 'upload' was called.");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("You've sent: " +  querystring.parse(postData).text);
-	response.end();
+	  console.log("Request handler 'upload' was called.");
+	  response.writeHead(200, {"Content-Type": "text/plain"});
+	  response.write("You've sent the text: "+
+	  querystring.parse(postData).text);
+	  response.end();
+}
+
+function show(response, postData) {
+  	console.log("Request handler 'show' was called.");
+  	fs.readFile("/tmp/test.png", "binary", function(error, file) {
+	    if(error) {
+	      	response.writeHead(500, {"Content-Type": "text/plain"});
+	      	response.write(error + "\n");
+	      	response.end();
+	    } else {
+	      	response.writeHead(200, {"Content-Type": "image/png"});
+	      	response.write(file, "binary");
+	      	response.end();
+	    }
+  	});
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
